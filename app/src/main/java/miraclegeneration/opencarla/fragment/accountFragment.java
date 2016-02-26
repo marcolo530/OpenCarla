7b863543614b860d258eb8267b5b179e0b71b301
marcolo530 <s1100113@ouhk.edu.hk>
2016-02-26 17:19:17 +0800

    26/2/2016

---
+++ app/src/main/java/miraclegeneration/opencarla/fragment/accountFragment.java
@@ -0,0 +1,26 @@
+package miraclegeneration.opencarla.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import miraclegeneration.opencarla.R;
+
+/**
+ * Created by kenex on 10/1/2016.
+ */
+public class accountFragment extends Fragment {
+    @Nullable
+    //method inflate the view
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_account, container, false);//first parameter resource id, second is view group, third is attach to root or not
+        return rootView;
+    }
+
+
+
+}
